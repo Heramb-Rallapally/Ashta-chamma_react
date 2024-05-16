2c27ed5d8385e125689a6f794cdc7b7fcb1f4899
Heramb <hs22csb0f19@student.nitw.ac.in>
2024-05-16 08:10:52 +0530

    adding my initial layout files

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Images_front from './Total.jsx';
+import './App.css'; // Import the CSS file
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello!</h1>
+      <Images_front />
+    </div>
+  );
+}
+
+export default App;

817ae6c085e7919b0f40927a939af9851f484ed3
Heramb <hs22csb0f19@student.nitw.ac.in>
2024-05-16 08:50:22 +0530

    adding files

--- SoundEffect.jsx
+++ SoundEffect.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import Sound from 'react-sound';
+import React, { useRef } from 'react';
 
 function SoundEffect() {
+  const audioRef = useRef(null);
+
+  const playSound = () => {
+    audioRef.current.play();
+  };
+
   return (
-    <Sound
-      url="D:\Ashta chamma\REACT_CODES\vite-project\src.mp3"
-      playStatus={Sound.status.PLAYING}
-      volume={50}
-    />
+    <div>
+      <button onClick={playSound}>Play Sound</button>
+      <audio ref={audioRef}>
+      <source src="/Rock_music.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
   );
 }
 

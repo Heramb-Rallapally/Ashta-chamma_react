5cd229dd53fe217626f4ceb274b27ff918df3de7
Heramb <hs22csb0f19@student.nitw.ac.in>
2024-05-17 09:49:12 +0530

    button thing done

--- App.jsx
+++ App.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Images_front from './Total.jsx';
 import './App.css'; 
 
 function App() {
+  const [imagesVisible, setImagesVisible] = useState(false); 
+
+  const handleButtonClick = () => {
+    setImagesVisible(!imagesVisible); 
+  };
+
   return (
     <div className="container">
       <h1 className='Heading_app'>Random Gavullu</h1>
-      <Images_front/>
+      {imagesVisible && <Images_front />} {}
+      <button onClick={handleButtonClick}>Show Random Images</button>
     </div>
   );
 }

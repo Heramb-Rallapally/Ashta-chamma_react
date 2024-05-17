8bcf36cee540a6c792a48c344a476a0aa1586afa
Heramb <hs22csb0f19@student.nitw.ac.in>
2024-05-17 09:29:49 +0530

    pulled and merged

--- App.jsx
+++ App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Images_front from './Total.jsx';
 import './App.css'; 
-import SoundEffect from './SoundEffect.jsx';
 
 function App() {
   return (
     <div className="container">
       <h1 className='Heading_app'>Random Gavullu</h1>
-      <SoundEffect />
+      <Images_front/>
     </div>
   );
 }
